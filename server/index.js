136ada18a786031fdbbe44429c3e22abde04e0e0
Arihant <arihantjain1208@gmail.com>
2023-10-10 17:36:24 +0530

    testing CD

--- server/index.js
+++ server/index.js
@@ -13,8 +13,6 @@ app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
 
-// Connect to MongoDB
-// DONT MISUSE THIS THANKYOU!!
 mongoose.connect('mongodb+srv://arihantjain1208:dpMyocasJ92sASfK@cluster0.siminoh.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp', { dbName: "courses" });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
